d95a69e4a9d856360db67a119b641443e06e29fd
chirag <chirag@healthcloudai.com>
2020-07-16 20:38:05 +0530

    fetched and displayed data fetched from api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
-// import Cards from "./components/Cards/Cards";
-// import Charts from "./components/Charts/Charts";
-// import CountryPicker from "./components/CountryPicker/CountryPicker";
-
 import { Cards, Charts, CountryPicker } from './components';
-import styles from 'App.module.css';
+import styles from './App.module.css';
+import { fetchData } from './api';
 
 class App extends React.Component{
+    state = {
+        data: {},
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({data: fetchedData});
+    }
+
     render(){
+        const{ data } = this.state
+
         return(
             <div className={styles.container}>
-                <Cards />
+                <Cards data={data}/>
                 <CountryPicker />
                 <Charts />
                 <h1>App</h1>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,46 @@
 import React from "react";
+import { Card, CardContent, Typography, Grid} from "@material-ui/core";
+import CountUp from "react-countup";
 
-const Cards = () => {
+import styles from './Cards.modules.css';
+
+
+const Cards = ({data: {confirmed, recovered , deaths, lastUpdate}}) => {
+    if(!confirmed || !recovered || !deaths || !lastUpdate){
+        return 'Loading';
+    }
+
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color={"textSecondary"} gutterBottom> Infected</Typography>
+                        <Typography variant={"h5"}>
+                            {/*<CountUp */}
+                            {confirmed.value}</Typography>
+                        <Typography color={"textSecondary"}> REAL DATE</Typography>
+                        <Typography variant={"body2"}>Number of Active Cases of Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color={"textSecondary"} gutterBottom> Recovered</Typography>
+                        <Typography variant={"h5"}>{recovered.value}</Typography>
+                        <Typography color={"textSecondary"}> REAL DATE</Typography>
+                        <Typography variant={"body2"}>Number of Recoveries</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color={"textSecondary"} gutterBottom> Deaths</Typography>
+                        <Typography variant={"h5"}>{deaths.value}</Typography>
+                        <Typography color={"textSecondary"}> REAL DATE</Typography>
+                        <Typography variant={"body2"}>Number of Deaths from Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
